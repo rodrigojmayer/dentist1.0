ced5a16e70eb1e4e838da736cb5f1a2056190d73
rodrigojmayer <rodrigojmayer@gmail.com>
2026-06-03 20:02:03 -0300

    isAdminPage change

--- components/admin/admin-dashboard.tsx
+++ components/admin/admin-dashboard.tsx
@@ -145,7 +145,7 @@ export function AdminDashboard() {
           </div>
         </div>
       </header> */}
-      <Header isAdmin={true} />
+      <Header isAdminPage={true} />
       {/* <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <div className="flex items-center justify-between mb-8">
           <div>
